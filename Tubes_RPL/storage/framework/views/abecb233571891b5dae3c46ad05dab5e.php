de5b048fba313d50fecddbca001f4cb92a7e2589
Lauratsanaullailla <tsnalauralailla@gmail.com>
2024-05-03 15:13:57 +0700

    Laura-controller

---
+++ Tubes_RPL/storage/framework/views/abecb233571891b5dae3c46ad05dab5e.php
@@ -0,0 +1,15 @@
+<div style="font-family: Arial, sans-serif; max-width: 400px; margin: auto;">
+    <h1 style="color: black;">IKAN</h1>
+    <h2 style="color: grey;">Masuk Akun Anda</h2>
+    
+    <form>
+        <input type="text" placeholder="Nama" style="width: 100%; padding: 10px; margin-bottom: 10px; border: 1px solid grey;">
+        <input type="email" placeholder="Email" style="width: 100%; padding: 10px; margin-bottom: 10px; border: 1px solid grey;">
+        <input type="password" placeholder="Password" style="width: 100%; padding: 10px; margin-bottom: 20px; border: 1px solid grey;">
+        <button type="submit" style="width: 100%; padding: 10px; background-color: lightblue; border: none;">Selanjutnya</button>
+    </form>
+
+    <p style="color: black;">- atau -</p>
+    <a href="#" style="color: black;">Sudah Memiliki Akun? Masuk</a>
+</div>
+<?php /**PATH D:\TUBES RPL\Tubes_RPL\resources\views/landing/signup.blade.php ENDPATH**/ ?>
