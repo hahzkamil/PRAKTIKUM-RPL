ae751bf80e573bae6aaf4ce5c03be899878604f1
ghifaryall <ghifary.m.al18@gmail.com>
2024-05-03 13:53:22 +0700

    Merge branch 'main' of https://github.com/hahzkamil/Projek-RPL
    # Conflicts:
    #	Tubes_RPL/routes/web.php

---
+++ Projek-RPL/storage/framework/views/0e4f204dd05e3c6a727b9c9717045a30.php
@@ -0,0 +1,34 @@
+<!-- Nav -->
+<nav class="navbar nav-underline navbar-expand-lg navbar-black" style="background-color: #6AB4DD;">
+    <a class="navbar-brand" href="#">
+    </a>
+    <div class="container justify-content-center">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse flex-grow-0" id="navbarNav">
+            <ul class="navbar-nav text-center" >
+                <li class="nav-item">
+                    <a class="nav-link"  href="#"><strong>Catalog</strong></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><strong>Daftar Mitra</strong></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><strong>Perkiraan cuaca</strong></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><strong>About Us</strong></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <ul class="nav justify-content-end">
+        <li class="nav-item">
+            <a class="btn-sm" aria-current="page" href="Sign in">Log Out</a>
+        </li>
+    </ul>
+</nav>
+
+<!-- Nav End --><?php /**PATH C:\Users\HP\Documents\Projek-RPL\Projek-RPL\resources\views/layout/navbar.blade.php ENDPATH**/ ?>
---
+++ Projek-RPL/storage/framework/views/1f1749e66251765c71adf05d6b2d81c9.php
@@ -0,0 +1,4 @@
+<?php $__env->startSection('title'); ?>
+<title>Dashboard</title>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layout.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\HP\Documents\Projek-RPL\Projek-RPL\resources\views/welcome.blade.php ENDPATH**/ ?>
