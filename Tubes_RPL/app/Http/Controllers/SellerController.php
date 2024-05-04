a824d58b2da307b173b3590bc544e100679111fd
gaitsazahirashafa <shafagaitsazahira@gmail.com>
2024-05-04 18:32:09 +0700

    coba

---
+++ Tubes_RPL/app/Http/Controllers/SellerController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Seller;
+use Illuminate\Http\Request;
+
+class SellerController extends Controller
+{
+    public function index()
+    {
+        $seller = Seller::all();
+
+        return view("sellerList.index"(["seller"->compact($seller)]));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Seller $seller)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Seller $seller)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Seller $seller)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Seller $seller)
+    {
+        //
+    }
+}
