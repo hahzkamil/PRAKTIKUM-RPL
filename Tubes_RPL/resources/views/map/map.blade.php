efe5329982086c335181695c0f61299b31662d59
hahzkamil <hafizh.kamil48@gmail.com>
2024-05-31 19:05:05 +0700

    tambahin dikit

--- Tubes_RPL/resources/views/map/map.blade.php
+++ Tubes_RPL/resources/views/map/map.blade.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Add Google Map in Laravel - shouts.dev</title>
+    <title>Dashboard</title>
 
     <style type="text/css">
         #map {
