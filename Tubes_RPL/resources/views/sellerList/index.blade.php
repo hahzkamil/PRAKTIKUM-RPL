c4f47491aedf3738b233f7502e843f8e3ff33746
mutiarafanny <hi.mutiarafanny@gmail.com>
2024-06-12 20:41:30 +0700

    view seller list

--- Tubes_RPL/resources/views/sellerList/index.blade.php
+++ Tubes_RPL/resources/views/sellerList/index.blade.php
@@ -1,39 +1,21 @@
 @extends('layout.main')
 
 @section('content')
-<h1 class="font-weight-bold d-flex justify-content-center" img src="https://img.wallmur.net/@wmp/_826ax660/s/s/ss1109394542_crop.jpg" class="img-thumbnail">Seller List</h1>
-<br>
-<div class="container">
+<h1 class="font-weight-bold text-center mt-5">Daftar Penjual</h1>
+<div class="container mt-5">
     <div class="row">
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Seller A</h5>
-                    <p class="card-text">Bojongsoang, Dayeuhkolot, Jawa Barat</p>
-                    <a href="" class="btn btn-primary">View Details</a>
+        @foreach ($sellers as $seller)
+            <div class="col-md-4 mb-4">
+                <div class="card shadow">
+                    <img src="{{ $seller->image }}" class="card-img-top" alt="Foto Penjual">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $seller->name }}</h5>
+                        <p class="card-text">{{ $seller->address }}</p>
+                        <a href="{{ route('seller.details', $seller->id) }}" class="btn btn-primary">Lihat Detail</a>
+                    </div>
                 </div>
             </div>
-        </div>
-
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Seller B</h5>
-                    <p class="card-text">Baleendah, Dayeuhkolot, Jawa Barat</p>
-                    <a href="" class="btn btn-primary">View Details</a>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Seller C</h5>
-                    <p class="card-text">Ciganitri, Dayeuhkolot, Jawa Barat</p>
-                    <a href="" class="btn btn-primary">View Details</a>
-                </div>
-            </div>
-        </div>
+        @endforeach
     </div>
 </div>
 @endsection
