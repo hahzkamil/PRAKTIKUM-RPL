f893addff88d9f228ea5585ddbfde450e8d3a3f2
ZhafranVI <Zhafranvirmandailham11@gmail.com>
2024-06-13 12:12:39 +0700

    Final bgt

--- Tubes_RPL/resources/views/bacaartikel/detail-artikel.blade.php
+++ Tubes_RPL/resources/views/bacaartikel/detail-artikel.blade.php
@@ -13,7 +13,7 @@
 
       <div class="row">
       <div style="display: flex; justify-content: center;">
-            <img src="{{ asset('storage/artikel/'. $artikel->photo) }}" class="my-4" alt="Article Image" style="width: 500px; height: 450px;">
+            <img src="{{ $artikel->photo }}" class="card-img-top my-4" alt="Article Image" style="width: 500px; height: 450px;">
         </div>
         <div class="card-body">
             <p class="card-text">{!! $artikel -> body !!}</p>
