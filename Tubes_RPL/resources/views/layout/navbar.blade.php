7bea183240a799f3c4d81f631e27112e115d6956
ZhafranVI <Zhafranvirmandailham11@gmail.com>
2024-06-13 14:08:33 +0700

    no message

--- Tubes_RPL/resources/views/layout/navbar.blade.php
+++ Tubes_RPL/resources/views/layout/navbar.blade.php
@@ -25,6 +25,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/show-artikel"><strong>News</strong></a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/events"><strong>Events</strong></a>
+                </li>
             </ul>
         </div>
     </div>
